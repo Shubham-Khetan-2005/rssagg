e570643f25d46e026b783a32e4b34d78649e55b4
Shubham Khetan <janukhetan2005@gmail.com>
2024-12-26 17:01:07 +0530

    Database connected

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/Shubham-Khetan-2005/rssagg
 go 1.23.4
 
 require (
-	github.com/go-chi/chi v1.5.5 // indirect
-	github.com/go-chi/chi/v5 v5.2.0 // indirect
-	github.com/go-chi/cors v1.2.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.2.0
+	github.com/go-chi/cors v1.2.1
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
 )
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Shubham-Khetan-2005/rssagg/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type apiConfig struct{
+	DB *database.Queries
+}
 func main() {
 	godotenv.Load()
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT must be set")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if portString == "" {
+		log.Fatal("DB_URL not found in env")
+	}
+
+	conn , err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Unable to connect to database:", err)
+	}
+
+	queries := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: queries,
 	}
 
 	router := chi.NewRouter()
@@ -33,6 +55,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/error", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -43,7 +66,7 @@ func main() {
 
 	fmt.Printf("Listening on port %s\n", portString)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
